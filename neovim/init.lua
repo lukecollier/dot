916bfa0a49988e8ca00ead963e5af705aa0cbc5f
lukecollier <me@lukecollier.dev>
2023-11-10 23:53:00 +0100

    feat: Uses lsp or git project root if it exists

--- neovim/init.lua
+++ neovim/init.lua
@@ -142,8 +142,27 @@ require("lazy").setup({
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+      local find_files = function(opts)
+        opts = opts or {}
+        opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        if #vim.lsp.get_active_clients() == 1 and vim.lsp.get_active_clients()[1].config then
+          opts.cwd = vim.lsp.get_active_clients()[1].config.root_dir
+        end
+        builtin.find_files(opts)
+      end
+
+      local live_grep = function(opts)
+        opts = opts or {}
+        opts.cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+        if #vim.lsp.get_active_clients() == 1 and vim.lsp.get_active_clients()[1].config then
+          opts.cwd = vim.lsp.get_active_clients()[1].config.root_dir
+        end
+        builtin.find_files(opts)
+      end
+
+      vim.keymap.set('n', '<leader>ff', find_files, {})
+      vim.keymap.set('n', '<leader>fg', live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       require('telescope').setup {}
